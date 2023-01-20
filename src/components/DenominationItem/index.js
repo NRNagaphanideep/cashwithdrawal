c8690625a31c8d626cb86b74e256ae81d35354c4
Nagaphanideep <95171605+NRNagaphanideep@users.noreply.github.com>
2023-01-20 15:09:26 +0530

    Update index.js

--- src/components/DenominationItem/index.js
+++ src/components/DenominationItem/index.js
@@ -1,15 +1,20 @@
-// Write your code here
 import './index.css'
 
 const DenominationItem = props => {
-  const {denominaionDetails, updateBalance} = props
-  const {value} = denominaionDetails
+  const {denominationDetails, updateBalance} = props
+  const {value} = denominationDetails
+
   const onClickDenomination = () => {
     updateBalance(value)
   }
+
   return (
     <li className="denomination-item">
-      <button type="button" className="button" onClick={onClickDenomination}>
+      <button
+        type="button"
+        className="denomination-button"
+        onClick={onClickDenomination}
+      >
         {value}
       </button>
     </li>
